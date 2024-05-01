6bfbd714b87cfbf28e39b720aec945fac2a8c623
mugishab2020 <mugishab2020@gmail.com>
2024-05-01 19:46:35 +0200

    We got this but in REdis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,7 +4,36 @@
 
 import redis
 import uuid
+from functools import wraps
 from typing import Union, Callable
+
+
+def call_history(method: Callable) -> Callable:
+    '''function for making call history'''
+    key = method.__qualname__
+    inputs = key + ":inputs"
+    outputs = key + ":outputs"
+
+    @wraps(method)
+    def wrapper_hist(self, *args, **kwargs):
+        '''Wrapper for pushing the resultds the the redis lists'''
+        self._redis.rpush(inputs, str(args))
+        data = method(self, *args, **kwargs)
+        self._redis.rpush(outputs, str(data))
+        return data
+
+    return wrapper_hist
+
+
+def count_calls(method: Callable) -> Callable:
+    '''function to count the number of calls to a method'''
+    @wraps(method)
+    def wrapper(self, *args, **kwargs) -> None:
+        '''returns the number of calls'''
+        key = method.__qualname__
+        self._redis.incr(key)
+        return method(self, *args, **kwargs)
+    return wrapper
 
 
 class Cache:
@@ -12,6 +41,8 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
+    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
